e2ac2e4af86ace3c7d276e250038fc9087301824
vanbrayne <lars.lindberg@xlent.se>
2015-04-18 22:54:50 +0200

    Found some bugs.

--- src/main/java/net/eithon/library/plugin/ConfigurableCommand.java
+++ src/main/java/net/eithon/library/plugin/ConfigurableCommand.java
@@ -1,6 +1,7 @@
 package net.eithon.library.plugin;
 
 import net.eithon.library.extensions.EithonPlugin;
+import net.eithon.library.misc.Debug.DebugPrintLevel;
 
 import org.bukkit.Bukkit;
 
@@ -13,6 +14,7 @@ public class ConfigurableCommand extends ConfigurableFormat{
 	
 	public void execute(Object... args) {
 		String command = getMessage(args);
+		this._eithonPlugin.getDebug().debug(DebugPrintLevel.MAJOR, "/%s", command);
 		this._eithonPlugin.getJavaPlugin().getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), command);
 	}
 }
