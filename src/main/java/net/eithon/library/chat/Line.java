f572c9a508b2891e0fc8ffcf0cd252c6c8a05ba7
Lars Lindberg <lars.lindberg@me.com>
2015-04-29 01:34:50 +0200

    Major refactoring of Paginator.

---
+++ src/main/java/net/eithon/library/chat/Line.java
@@ -0,0 +1,110 @@
+package net.eithon.library.chat;
+
+import java.util.Stack;
+
+import org.bukkit.ChatColor;
+
+class Line {
+	private StringBuilder _content;
+	private int _widthInPixels;
+	private int _numberOfVisibleCharacters;
+	private boolean _hasPendingSoftHyphen;
+	private String _activeChatColorCharacters;
+	private boolean _isFirstLine;
+
+	Line()  {
+		this._activeChatColorCharacters = "";
+		reset();
+		this._isFirstLine = true;
+	}
+
+	boolean hasPendingSoftHyphen() { return this._hasPendingSoftHyphen; }
+
+	void setHasPendingSoftHyphen(boolean value) { this._hasPendingSoftHyphen = value; }
+
+	boolean hasContent() { return this._numberOfVisibleCharacters > 0; }
+
+	int getWidthInPixels() { return this._widthInPixels; }	
+
+	public boolean isFirstLine() { return this._isFirstLine; }
+	
+	@Override
+	public String toString() { return this._content.toString(); }
+
+	void reset() {
+		this._content = new StringBuilder();
+		this._widthInPixels = 0;
+		this._numberOfVisibleCharacters = 0;
+		this._hasPendingSoftHyphen = false;
+		// Add all active color codes
+		for (char chatColor : this._activeChatColorCharacters.toCharArray()) {
+			this._content.append(ChatColor.COLOR_CHAR);
+			this._content.append(chatColor);
+		}
+		this._isFirstLine = false;
+	}
+
+	void add(Word word) {		
+		updateChatColors(word.toString());
+		this._content.append(word.toString());
+		this._widthInPixels += word.getWidthInPixels();
+		this._numberOfVisibleCharacters += word.getVisibleCharacters();
+		this._hasPendingSoftHyphen = false;
+	}
+	
+	void addAndResetWord(Word word) {
+		add(word);
+		word.reset();
+	}
+
+	void add(char c, int characterInPixels, boolean isVisibleCharacter) {
+		this._content.append(c);
+		this._widthInPixels += characterInPixels;
+		if (isVisibleCharacter) this._numberOfVisibleCharacters ++;
+		this._hasPendingSoftHyphen = false;
+	}
+
+	private void updateChatColors(String word) {
+		String newActiveChatColorCharacters = "";
+
+		// Get a stack over new color characters in the word
+		Stack<Character> colorStack = new Stack<Character>();
+		boolean nextCharIsColorChar = false;
+		for (char c : word.toCharArray()) {
+			if (c == ChatColor.COLOR_CHAR) {
+				nextCharIsColorChar = true;
+				continue;
+			}
+			if (nextCharIsColorChar) {
+				colorStack.push(c);
+				nextCharIsColorChar = false;
+			}
+		}
+		for (char c : this._activeChatColorCharacters.toCharArray()) {
+			colorStack.push(c);
+		}
+
+		while (!colorStack.isEmpty()) {
+			Character c = colorStack.pop();
+			if (isOverridden(c, newActiveChatColorCharacters)) continue;
+			newActiveChatColorCharacters = Character.toString(c) + newActiveChatColorCharacters;
+		}
+		this._activeChatColorCharacters = newActiveChatColorCharacters;
+	}
+
+	private static boolean isOverridden(Character c, String chatColors) {
+		ChatColor newCharacter = ChatColor.getByChar(c);
+		for (char colorCharacter : chatColors.toCharArray()) {
+			if (c == colorCharacter) return true;
+			if (isOverridden(newCharacter, ChatColor.getByChar(colorCharacter))) return true;
+		}
+		return false;
+	}
+
+	private static boolean isOverridden(ChatColor newCharacter, ChatColor existingCharacter) {
+		if (existingCharacter == ChatColor.RESET) return true;
+		if (newCharacter.isFormat()) return false;
+		if (existingCharacter.isColor()) return true;
+		return false;
+	}
+}
--- src/main/java/net/eithon/library/chat/Paginator.java
+++ src/main/java/net/eithon/library/chat/Paginator.java
@@ -45,210 +45,27 @@ public class Paginator {
 	 * @return A single chat page.
 	 */
 	public static Page[] paginate(String[] inputLines, int lineLength, int pageHeight) {
-		final int hardHyphenPixels = FontPixels.get().pixelWidth(HARD_HYPHEN);
+		inputLines = makeHardBreaksEasier(inputLines);
 		List<Page> pages = new ArrayList<Page>();
 		List<String> outputLines = new LinkedList<String>();
 		for (String inputLine : inputLines) {
-			char[] rawChars = (inputLine + ' ').toCharArray(); // add a trailing space to trigger pagination
-			StringBuilder word = new StringBuilder();
-			StringBuilder outputLine = new StringBuilder();
-			int outputLlineInPixels = 0;
-			int wordInPixels = 0;
-			int outputLineVisibleCharacters = 0;
-			int wordVisibleCharacters = 0;
-			boolean lastWordHadSoftHyphenAfterIt = false;
-			String activeChatColorCharacters = "";
-			for (int i = 0; i < rawChars.length; i++) {
-				char c = rawChars[i];
-				boolean lastBreakWasAutoBreak = false;
-
-				if (c == ChatColor.COLOR_CHAR) {
-					word.append(c);
-					i++;
-					c = rawChars[i];
-					word.append(c);
-					continue;
-				}
-
-				int characterInPixels = FontPixels.get().pixelWidth(c);
-				if (c == SOFT_HYPHEN) {
-					characterInPixels = hardHyphenPixels;
-				}
-				int newLineLength = outputLlineInPixels + wordInPixels + characterInPixels;
-
-				// Time for a line break?
-				boolean timeToBreak = false;
-				if (isHardBreak(c)) {
-					// A "hard" break, such as LINE_BREAK or PAGE_BREAK
-					timeToBreak = true;
-					lastBreakWasAutoBreak = false;				
-				} else if (newLineLength > lineLength) {
-					timeToBreak = true;
-					lastBreakWasAutoBreak = true;
-				}
-
-				if (timeToBreak) {
-					if (!lastBreakWasAutoBreak || (c == SPACE)) {
-						if (word.length() > 0) {
-							activeChatColorCharacters = updateChatColors(activeChatColorCharacters, word.toString());
-							outputLine.append(word);
-							outputLlineInPixels += wordInPixels;
-							outputLineVisibleCharacters += wordVisibleCharacters;
-							word = new StringBuilder();
-							wordInPixels = 0;
-							wordVisibleCharacters = 0;
-							lastWordHadSoftHyphenAfterIt = false;
-						}
-					} else if (lastWordHadSoftHyphenAfterIt) {
-						outputLine.append(HARD_HYPHEN);
-						outputLlineInPixels += hardHyphenPixels;
-						outputLineVisibleCharacters += 1;
-						lastWordHadSoftHyphenAfterIt = false;
-					}
-					outputLines.add(outputLine.toString());
-					if ((outputLines.size() >= pageHeight) || (c == PAGE_BREAK)) {
-						pages.add(new Page(outputLines.toArray(new String[outputLines.size()]), 1, 1));
-						outputLines = new LinkedList<String>();
-					}
-					outputLine = new StringBuilder();
-					outputLlineInPixels = 0;
-					outputLineVisibleCharacters = 0;
-					if (lastBreakWasAutoBreak) {
-						// Add all active color codes
-						for (char chatColor : activeChatColorCharacters.toCharArray()) {
-							outputLine.append(ChatColor.COLOR_CHAR);
-							outputLine.append(chatColor);
-						}
-					} else {
-						activeChatColorCharacters = "";
-					}
-				}
-
-				switch (c) {
-				case LINE_BREAK:
-				case PAGE_BREAK:
-					// Already taken care of
-					break;
-				case HARD_HYPHEN:
-				case SPACE:
-					if (wordVisibleCharacters > 0) {
-						// This word brake is after an earlier word
-						word.append(c);
-						wordInPixels += characterInPixels;
-						activeChatColorCharacters = updateChatColors(activeChatColorCharacters, word.toString());
-						outputLine.append(word);
-						outputLlineInPixels += wordInPixels;
-						outputLineVisibleCharacters += wordVisibleCharacters;
-						word = new StringBuilder();
-						wordInPixels = 0;
-						wordVisibleCharacters = 0;
-						lastWordHadSoftHyphenAfterIt = false;
-					} else if (!lastBreakWasAutoBreak) {
-						// This is an indenting space/hyphen
-						outputLine.append(c);
-						outputLlineInPixels += characterInPixels;
-					} else {
-						// This is a space in the beginning of the line, after an auto break
-						// Ignore the space
-					}
-					break;
-				case SOFT_HYPHEN:
-					if (wordVisibleCharacters > 0) {
-						activeChatColorCharacters = updateChatColors(activeChatColorCharacters, word.toString());
-						outputLine.append(word);
-						outputLlineInPixels += wordInPixels;
-						outputLineVisibleCharacters += wordVisibleCharacters;
-						word = new StringBuilder();
-						wordInPixels = 0;
-						wordVisibleCharacters = 0;
-						lastWordHadSoftHyphenAfterIt = true;
-					}
-					break;
-				default:
-					word.append(c);
-					wordVisibleCharacters++;
-					wordInPixels += characterInPixels;
-					break;
-				}
-			}
-			if (word.length() > 0) {
-				activeChatColorCharacters = updateChatColors(activeChatColorCharacters, word.toString());
-				outputLine.append(word);
-				outputLlineInPixels += wordInPixels;
-				outputLineVisibleCharacters += wordVisibleCharacters;
-				word = new StringBuilder();
-				wordInPixels = 0;
-				wordVisibleCharacters = 0;
-				lastWordHadSoftHyphenAfterIt = false;
-			}
-			outputLines.add(outputLine.toString());
-			if (outputLines.size() >= pageHeight) {
-				pages.add(new Page(outputLines.toArray(new String[outputLines.size()]), 1, 1));
-				outputLines = new LinkedList<String>();
-			}
-			outputLine = new StringBuilder();
-			outputLlineInPixels = 0;
-			outputLineVisibleCharacters = 0;
-			if (outputLineVisibleCharacters > 0) outputLines.add(outputLine.toString());
 		}
 
 		if (outputLines.size() > 0) {
 			pages.add(new Page(outputLines.toArray(new String[outputLines.size()]), 1, 1));
 		}
 
-		return pages.toArray(new Page[pages.size()]);
+		return pages.toArray(new Page[0]);
+	}
+	
+	if ((outputLines.size() >= pageHeight) || (c == PAGE_BREAK)) {
+		pages.add(new Page(outputLines.toArray(new String[outputLines.size()]), 1, 1));
+		outputLines = new LinkedList<String>();
+	}
+	if (outputLines.size() >= pageHeight) {
+		pages.add(new Page(outputLines.toArray(new String[outputLines.size()]), 1, 1));
+		outputLines = new LinkedList<String>();
 	}
 
-	private static String updateChatColors(String activeChatColorCharacters,
-			String word) {
-		String chatColors = "";
 
-		// Get a stack over new color characters in the word
-		Stack<Character> colorStack = new Stack<Character>();
-		boolean nextCharIsColorChar = false;
-		for (char c : word.toCharArray()) {
-			if (c == ChatColor.COLOR_CHAR) {
-				nextCharIsColorChar = true;
-				continue;
-			}
-			if (nextCharIsColorChar) {
-				colorStack.push(c);
-				nextCharIsColorChar = false;
-			}
-		}
-		for (char c : activeChatColorCharacters.toCharArray()) {
-			colorStack.push(c);
-		}
-		
-		while (!colorStack.isEmpty()) {
-			Character c = colorStack.pop();
-			if (isOverridden(c, chatColors)) continue;
-			chatColors = Character.toString(c) + chatColors;
-		}
-		return chatColors;
-	}
-
-	private static boolean isOverridden(Character c, String chatColors) {
-		ChatColor newCharacter = ChatColor.getByChar(c);
-		for (char colorCharacter : chatColors.toCharArray()) {
-			if (c == colorCharacter) return true;
-			if (isOverridden(newCharacter, ChatColor.getByChar(colorCharacter))) return true;
-		}
-		return false;
-	}
-
-	private static boolean isOverridden(ChatColor newCharacter, ChatColor existingCharacter) {
-		if (existingCharacter == ChatColor.RESET) return true;
-		if (newCharacter.isFormat()) return false;
-		if (existingCharacter.isColor()) return true;
-		return false;
-	}
-
-	private static boolean isWordBreak(char c) {
-		return (c == SPACE) || (c == HARD_HYPHEN) || (c == SOFT_HYPHEN);
-	}
-
-	private static boolean isHardBreak(char c) {
-		return (c == LINE_BREAK) || (c == PAGE_BREAK);
-	}
 }
