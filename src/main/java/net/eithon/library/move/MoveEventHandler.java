8eed0bbb26b6cde3154b7e2e7d71b4c6525e5b79
vanbrayne <lars.lindberg@xlent.se>
2015-05-02 00:58:10 +0200

    Smarter PlayerMovedEvent handler.

--- src/main/java/net/eithon/library/move/MoveEventHandler.java
+++ src/main/java/net/eithon/library/move/MoveEventHandler.java
@@ -2,6 +2,7 @@ package net.eithon.library.move;
 
 
 
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerMoveEvent;
 
@@ -11,12 +12,17 @@ public class MoveEventHandler {
 	public static PlayerCollection<BlockMover> _movers = new PlayerCollection<BlockMover>();
 	public static void handle(PlayerMoveEvent event) {
 		if (event.isCancelled()) return;
+		if (isSameBlock(event.getFrom().getBlock(), event.getTo().getBlock())) return;
 		BlockMover mover = _movers.get(event.getPlayer());
 		if (mover == null) return;
 		if (!mover.hasMoved(event.getTo())) return;
 		mover.informFollowers(event);
 	}
 	
+	private static boolean isSameBlock(Block from, Block to) {
+		return (from.getX() == to.getX()) && (from.getY() == to.getY()) && (from.getZ() == to.getZ());
+	}
+
 	public static void addBlockMover(Player player, IBlockMoverFollower follower) {
 		BlockMover mover = _movers.get(player);
 		if (mover == null) {
