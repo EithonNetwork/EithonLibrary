1b0fec251ea45d87fc4865dafde2e15d20fda22c
Lars Lindberg <lars.lindberg@me.com>
2015-05-24 20:30:04 +0200

    Added missing Configuration overload.

--- src/main/java/net/eithon/library/json/Converter.java
+++ src/main/java/net/eithon/library/json/Converter.java
@@ -28,6 +28,12 @@ import org.json.simple.JSONValue;
 public class Converter {
 	@SuppressWarnings("unchecked")	
 	
+	public static long toLong(Object o, long defaultValue)
+	{
+		if (o == null) return defaultValue;
+		return (long) o;
+	}
+	
 	@Deprecated // Use EithonLocation instead
 	public static JSONObject fromLocation(Location location, boolean withWorld)
 	{
