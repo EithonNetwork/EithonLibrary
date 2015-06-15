c206bc38c1e2b1188ca18b8370db3a0768229899
Lars Lindberg <lars.lindberg@me.com>
2015-06-15 23:06:11 +0200

    New alarm features.

--- src/main/java/net/eithon/library/time/Alarm.java
+++ src/main/java/net/eithon/library/time/Alarm.java
@@ -1,9 +1,10 @@
 package net.eithon.library.time;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 class Alarm {
-
+	private UUID _id;
 	private LocalDateTime _when;
 	private Runnable _task;
 	private boolean _hasBeenSetOff;
@@ -11,6 +12,7 @@ class Alarm {
 
 	Alarm(String name, LocalDateTime when, Runnable task)
 	{
+		this._id = UUID.randomUUID();
 		this._when = when;
 		this._task = task;
 		this._name = name;
@@ -22,9 +24,16 @@ class Alarm {
 		return this._when;
 	}
 	
-	String getName()
-	{
-		return this._name;
+	String getName() { return this._name; }
+	UUID getId() { return this._id; }
+	boolean isSame(UUID id) { return this._id == id; }
+	boolean hasBeenSetOff() { return this._hasBeenSetOff; }
+	
+	void reset(LocalDateTime when) {
+		synchronized(this) {
+			this._when = when;
+			this._hasBeenSetOff = false;
+		}
 	}
 
 	boolean maybeSetOff() 
