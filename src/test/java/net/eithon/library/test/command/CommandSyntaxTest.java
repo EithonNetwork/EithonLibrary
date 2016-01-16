6f2627bef0fe5411a46bb3e08d45b9f369c1941b
Lars Lindberg <lars.lindberg@me.com>
2016-01-16 12:16:18 +0100

    Added test cases.

---
+++ src/test/java/net/eithon/library/test/command/CommandSyntaxTest.java
@@ -0,0 +1,73 @@
+package net.eithon.library.test.command;
+
+import net.eithon.library.command.syntax.CommandSyntax;
+import net.eithon.library.command.syntax.ParameterSyntax;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CommandSyntaxTest {
+	@Test
+	public void rootOnly() 
+	{
+		// Do and verify
+		createRoot("root");
+	}
+	
+	@Test
+	public void subCommand() 
+	{
+		// Prepare
+		final String subName = "sub";
+		final String subCommand = String.format("%s", subName);
+		CommandSyntax root = createRoot("root");
+		
+		// Do and verify
+		createSubCommand(root, subName, subCommand);		
+	}
+	
+	@Test
+	public void parameter() 
+	{
+
+		// Prepare
+		final String subName = "sub";
+		final String parameterName = "parameter";
+		final String subCommand = String.format("%s <%s>", subName, parameterName);
+		final CommandSyntax root = createRoot("root");
+		
+		// Do
+		createSubCommand(root, subName, subCommand);		
+		
+		// Verify
+		final CommandSyntax sub = root.getSubCommand(subName);
+		final ParameterSyntax parameterSyntax = root.getParameterSyntax(parameterName);
+		Assert.assertNotNull(parameterSyntax);
+		Assert.assertEquals(parameterName, parameterSyntax.getName());
+	}
+
+	private CommandSyntax createRoot(String name) {
+		final CommandSyntax root = new CommandSyntax(name); 
+		Assert.assertNotNull(root);	
+		Assert.assertEquals(name,root.getName());
+		Assert.assertEquals(name,root.toString());
+		return root;
+	}
+
+	private CommandSyntax createSubCommand(CommandSyntax root, String name, String command) {
+		// Prepare
+		final String completeCommand = String.format("%s %s", root.toString(), command);
+		
+		// Do
+		root.addCommand(command);	
+		
+		// Verify
+		final CommandSyntax sub = root.getSubCommand(name);
+		Assert.assertNotNull(sub);	
+		Assert.assertEquals(name,sub.getName());		
+		Assert.assertEquals(command,sub.toString());
+		Assert.assertEquals(completeCommand,root.toString());
+		
+		return root;
+	}
+}
