05d4b51cb4118c6fd09678b68505ac6c83cb7bdb
vanbrayne <lars.lindberg@xlent.se>
2016-08-14 18:06:20 +0200

    New support for DB coding.

---
+++ src/main/java/net/eithon/library/mysql/DbLogic.java
@@ -0,0 +1,30 @@
+package net.eithon.library.mysql;
+
+import java.util.List;
+
+import net.eithon.library.exceptions.FatalException;
+import net.eithon.library.exceptions.TryAgainException;
+
+public abstract class DbLogic<T extends ITable> {
+	protected JDapper<T> jDapper;	
+	
+	public DbLogic(Class<T> type, final Database database) throws FatalException {
+		this.jDapper = new JDapper<T>(type, database);
+	}
+
+	public T get(final long id) throws FatalException, TryAgainException {
+		return this.jDapper.read(id);
+	}
+
+	public List<T> findAll() throws FatalException, TryAgainException {
+		return this.jDapper.readSomeWhere("1=1");
+	}
+	
+	public void update(T data) throws FatalException, TryAgainException {
+		this.jDapper.updateWhere(data, "id = ?", data.getId());
+	}
+
+	public void delete(long id) throws FatalException, TryAgainException {
+		this.jDapper.delete(id);
+	}
+}
---
+++ src/main/java/net/eithon/library/mysql/Table.java
@@ -0,0 +1,22 @@
+package net.eithon.library.mysql;
+
+public class Table implements ITable {
+	public long id;
+	private String tableName;
+	
+	public Table(String tableName){
+		this.tableName = tableName;
+	}
+	@Override
+	public long getId() {
+		// TODO Auto-generated method stub
+		return id;
+	}
+
+	@Override
+	public String getTableName() {
+		// TODO Auto-generated method stub
+		return this.tableName;
+	}
+
+}
