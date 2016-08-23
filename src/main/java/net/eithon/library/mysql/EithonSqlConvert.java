a39897a1e74526055ec10f548d648d66025970f6
vanbrayne <lars.lindberg@xlent.se>
2016-08-23 19:47:28 +0200

    Now checks for null

--- src/main/java/net/eithon/library/mysql/EithonSqlConvert.java
+++ src/main/java/net/eithon/library/mysql/EithonSqlConvert.java
@@ -4,18 +4,22 @@ import java.time.LocalDateTime;
 
 public class EithonSqlConvert {
 	public static LocalDateTime toLocalDateTime(java.sql.Date source) {
+		if (source == null) return null;
 		return source.toLocalDate().atStartOfDay();
 	}
 	
 	public static LocalDateTime toLocalDateTime(java.sql.Timestamp source) {
+		if (source == null) return null;
 		return source.toLocalDateTime();
 	}
 	
 	public static java.sql.Date toSqlDate(LocalDateTime source) {
+		if (source == null) return null;
 		return java.sql.Date.valueOf(source.toLocalDate());
 	}
 	
 	public static java.sql.Timestamp toSqlTimestamp(LocalDateTime source) {
+		if (source == null) return null;
 		return java.sql.Timestamp.valueOf(source);
 	}
 }
